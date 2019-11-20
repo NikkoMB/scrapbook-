87620e14279aa146682a922f8ef22def01355b01
bustillo.nikko@yahoo.com <bustillo.nikko@yahoo.com>
2019-11-20 12:52:45 -0500

    register and login html complete

---
+++ main.js
@@ -0,0 +1,12 @@
+'use strict'; 
+
+let email = document.getElementById('emailHelp'); 
+let password = document.getElementById('pass-error');
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+const invalidForm = () => {
+
+}
